10eab033a95c79d8c859a5f099c92c596649b4a3
Marcel <1412556777@qq.com>
2023-07-30 16:12:36 +0800

    8卡实验

---
+++ scripts/just_try7.sh
@@ -0,0 +1,19 @@
+
+# zjumocap上的实验脚本
+# ================对应 inb7 文件夹内的参数==========================
+# 使用例子   param1 为gpu编号  param2 为实验名
+
+# sh scripts/just_try.sh "7," "baseline_1"
+
+# 脚本会保存实验指标到metrics文件夹
+
+export GPUS=$1
+
+en=$2
+file="metrics/${en}.txt"
+
+for name in 377 386 387 392 393 394
+do
+    python train_net.py --cfg_file configs/inb7/inb_${name}.yaml exp_name ${en}_${name} gpus ${GPUS} silent False
+    python run.py --type evaluate --cfg_file configs/inb7/inb_${name}.yaml exp_name ${en}_${name} gpus ${GPUS} | grep -E 'mse:|psnr:|ssim:|lpips:'| awk -F ":" '{printf "%s,",$2}'  >> ${file} 
+done
